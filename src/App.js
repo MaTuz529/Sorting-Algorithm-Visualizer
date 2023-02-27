94617b24ec8ec86b698ccb1e144f1e4a4475a638
Matthew Tuzinowski <97304885+FatChickenJomma@users.noreply.github.com>
2023-02-26 21:03:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import Content from './Content';
 
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import SelectionSort from './pages/SelectionSort'
 import InsertionSort from './pages/InsertionSort'
 import QuickSort from './pages/QuickSort'
 import MergeSort from './pages/MergeSort'
-import HeapSort from './pages/HeapSort'
 import BubbleSort from './pages/BubbleSort'
 function App() {
   return(
     <Router>
       <Routes>
         <Route path="/" element={<Content />} />
-        <Route path="selectionsort" element={<SelectionSort />} />
         <Route path="insertionsort" element={<InsertionSort />} />
         <Route path="quicksort" element={<QuickSort />} />
         <Route path="mergesort" element={<MergeSort />} />
-        <Route path="heapsort" element={<HeapSort />} />
         <Route path="bubblesort" element={<BubbleSort />} />
       </Routes>
     </Router>
